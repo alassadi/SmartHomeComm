8a95a721112f494fb6ab7cd731d605c9474d93b7
jimmyjnssson <jimmyjnsson@gmail.com>
2019-01-06 18:29:35 +0100

    onFire method to detect a fire from arduino
    NOT TESTED, compares a string from readline() and then post to firebase

--- pythonread.py
+++ pythonread.py
@@ -2,9 +2,12 @@ import os
 import sys 
 import time
 import serial
+from firebase import firebase
 
 port = '/dev/ttyACM0'
 arduino = serial.Serial(port, 9600)
+
+firebase = firebase.FirebaseApplication('https://smarthome-3c6b9.firebaseio.com/', None) #Initiate Firebase
 
 #print(arduino.readline())
 
@@ -32,13 +35,32 @@ class Watcher(object):
             line = f.readline()
             print(line+" sent.")
             arduino.write(str.encode(line))
-            #Modification of Jimmy's method, write all responses we get from arduino to firebase. Firebase then sends it to clients via FCM.
-            #dataRead = arduino.readline()
-            from firebase import firebase
-            firebase = firebase.FirebaseApplication('linktosmarthouse', None) 
-            result = firebase.patch('/categoryinfirebase',{'Response':#dataRead}) 
-            print(result)
+
+
+   
+     
+
+    #Method to detect a fire, then write it to some table in Firebase. NOT TESTED
+    def update_onFire():
+        dataRead = arduino.readline()
+        time.sleep(1)
+        compareData = dataRead
+        if compareData. = "FIRE!!":
+            time.sleep(1)
+            data = {"": dataRead}
+            firebase.post('sometableinfirebase', data) #Post to some table in Firebase
             
+            print compareData
+        else:
+            print('Failed to get data from Arduino.')
+            time.sleep(10)
+
+        
+            
+        update_onFire()
+        time.sleep(5000) 
+
+      
 
 
 
