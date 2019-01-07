2598854c42b1ec29499bac5dbefd8c2e47088156
Grov12 <robinforsman@hotmail.com>
2019-01-07 19:17:01 +0100

    Backup

--- pythonread.py
+++ pythonread.py
@@ -1,16 +1,23 @@
 import os
 import sys 
 import time
+import requests, json
+import queue
+import threading
+import random
+# import serial
 
-#port = '/dev/ttyACM0'
-
+# port = '/dev/ttyACM0'
 # arduino = serial.Serial(port, 9600)
 
-#print(arduino.readline())
+# print(arduino.readline())
 
 class Watcher(object):
     running = True
-    refresh_milliSecond = 0.002
+    refresh_milliSecond = 0.010
+    
+    
+   
 
     # Constructor
     def __init__(self, watch_file, *args, **kwargs):
@@ -18,103 +25,99 @@ class Watcher(object):
         self.filename = watch_file
         self.args = args
         self.kwargs = kwargs
+        
+        thread = threading.Thread(target=self.checkFire, args=())
+        #thread2 = threading.Thread(target=self.checkWaterLeak, args=())
+        thread.daemon = True
+        #thread2.daemon = True
+        thread.start()
+        #thread2.start()
+        
+        
+        
+        
+       
 
-    
+    # Look for changes
     def look(self):
         stamp = os.stat(self.filename).st_mtime
         if stamp != self._cached_stamp:
             self._cached_stamp = stamp
-            # File has changed, so do something...
-            f = open(r'C:\\Users\\Robin\\Desktop\\kk.txt', 'r')
+           
+
+            filepath = 'C:\\Users\\robin\\Desktop\\Javascript\\tester12\\test.txt'
+            f = open(filepath, 'r')
             line = f.readline()
-            inputFromBase = line
-            print(inputFromBase)
-            if inputFromBase is '1':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('1 was sent to Arduino')
-            if inputFromBase is '2':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('2 was sent to Arduino')
-            if inputFromBase is '3':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('3 was sent to Arduino')
-            if inputFromBase is '4':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('4 was sent to Arduino')
-            if inputFromBase is '5':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('5 was sent to Arduino')
-            if inputFromBase is '6':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('6 was sent to Arduino')
-            if inputFromBase is '7':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('7 was sent to Arduino')
-            if inputFromBase is '8':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('8 was sent to Arduino')
-            if inputFromBase is '9':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('9 was sent to Arduino')
-            if inputFromBase == '10':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('10 was sent Arduino')
-            if inputFromBase == '11':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('11 was sent to Arduino')
-            if inputFromBase == '12':
-            	#arduino.write(b'<1>')
-            	#response_From_Arduino = arduino.readline()
-            	#print(response_From_Arduino)
-            	print('12 was sent to Arduino')
+            print(line+" sent.")
+            dataRead = line
+            from firebase import firebase
+            firebase = firebase.FirebaseApplication('https://fir-test-7f5bd.firebaseio.com/', None)
+            result = firebase.patch('/ResponseFromArduino',{'Response':'LAMP OFF'})
+            print(result)
+
+            
+           
+
+    
+    def checkFire(self):
+        #arduino.write(str.encode(19))
+        #dataRead = arduino.readline()
+        while True:
+         checka = random.randint(1,101)
+         if checka > 0:
+          #arduino.write(str.encode(19))
+          from firebase import firebase
+          firebase = firebase.FirebaseApplication('https://fir-test-7f5bd.firebaseio.com/', None)
+          result = firebase.patch('/ResponseFromArduino',{'Fire':checka})
+          time.sleep(100)
           
 
+    '''
+    def checkWaterLeak(self):
+        #arduino.write(str.encode(19))
+        #dataRead = arduino.readline()
+        while True:
+         checka = random.randint(1,101)
+         if checka > 0:
+          from firebase import firebase
+          firebase = firebase.FirebaseApplication('https://fir-test-7f5bd.firebaseio.com/', None)
+          result = firebase.patch('/ResponseFromArduino',{'Waterleak':checka})
+          time.sleep(100)
+          '''
+         
+         
+         
+
+         
+        
 
 
+         
 
-       
+             
+            
+         
+   # Keep watching in a loop
     def watch(self):
-        while self.running: 
-            try: 
-                # Look for changes
-                time.sleep(self.refresh_milliSecond) 
-                self.look() 
-            except KeyboardInterrupt: 
-                print('\nDone') 
-                break 
+ 
+        while self.running:
+            try:
+                time.sleep(self.refresh_milliSecond)
+                self.look()
+               
+                    
+            except KeyboardInterrupt:
+                print('\nDone')
+                break
             except FileNotFoundError:
-                print('No file found')
+                # Action on file not found
                 pass
-            except: 
+            except:
                 print('Unhandled error: %s' % sys.exc_info()[0])
 
 
+watch_file = 'C:\\Users\\robin\\Desktop\\Javascript\\tester12\\test.txt'
 
-
-watch_file = 'C:\\Users\\Robin\\Desktop\\kk.txt'
-
-
-watcher = Watcher(watch_file) 
-watcher.watch()  
+# watcher = Watcher(watch_file)  # simple
+watcher = Watcher(watch_file)  # also call custom action function
+watcher.watch()  # start the watch going
