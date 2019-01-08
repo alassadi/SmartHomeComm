fc772264fa3d0acd09e7512e963918595d8d961f
Grov12 <robinforsman@hotmail.com>
2019-01-08 21:37:31 +0100

    Tester threads.

--- pythonread.py
+++ pythonread.py
@@ -1,23 +1,20 @@
 import os
 import sys 
 import time
-import requests, json
-import queue
 import threading
 import random
-# import serial
+import serial
 
-# port = '/dev/ttyACM0'
-# arduino = serial.Serial(port, 9600)
 
-# print(arduino.readline())
+port = '/dev/ttyACM0'
+arduino = serial.Serial(port, 9600)
+
+
+print(arduino.readline())
 
 class Watcher(object):
     running = True
     refresh_milliSecond = 0.010
-    
-    
-   
 
     # Constructor
     def __init__(self, watch_file, *args, **kwargs):
@@ -25,17 +22,6 @@ class Watcher(object):
         self.filename = watch_file
         self.args = args
         self.kwargs = kwargs
-        
-        thread = threading.Thread(target=self.checkFire, args=())
-        #thread2 = threading.Thread(target=self.checkWaterLeak, args=())
-        thread.daemon = True
-        #thread2.daemon = True
-        thread.start()
-        #thread2.start()
-        
-        
-        
-        
        
 
     # Look for changes
@@ -43,69 +29,48 @@ class Watcher(object):
         stamp = os.stat(self.filename).st_mtime
         if stamp != self._cached_stamp:
             self._cached_stamp = stamp
+
            
 
-            filepath = 'C:\\Users\\robin\\Desktop\\Javascript\\tester12\\test.txt'
-            f = open(filepath, 'r')
-            line = f.readline()
-            print(line+" sent.")
-            dataRead = line
-            from firebase import firebase
-            firebase = firebase.FirebaseApplication('https://fir-test-7f5bd.firebaseio.com/', None)
-            result = firebase.patch('/ResponseFromArduino',{'Response':'LAMP OFF'})
-            print(result)
-
+            thread = threading.Thread(target=self.writeaAndRespond())
+            thread.daemon = True
             
-           
-
-    
-    def checkFire(self):
-        #arduino.write(str.encode(19))
-        #dataRead = arduino.readline()
-        while True:
-         checka = random.randint(1,101)
-         if checka > 0:
-          #arduino.write(str.encode(19))
-          from firebase import firebase
-          firebase = firebase.FirebaseApplication('https://fir-test-7f5bd.firebaseio.com/', None)
-          result = firebase.patch('/ResponseFromArduino',{'Fire':checka})
-          time.sleep(100)
-          
-
-    '''
-    def checkWaterLeak(self):
-        #arduino.write(str.encode(19))
-        #dataRead = arduino.readline()
-        while True:
-         checka = random.randint(1,101)
-         if checka > 0:
-          from firebase import firebase
-          firebase = firebase.FirebaseApplication('https://fir-test-7f5bd.firebaseio.com/', None)
-          result = firebase.patch('/ResponseFromArduino',{'Waterleak':checka})
-          time.sleep(100)
-          '''
-         
-         
-         
-
-         
+      
+            thread.start()
         
 
 
-         
 
-             
-            
-         
-   # Keep watching in a loop
+
+
+   
+    def writeaAndRespond(self):
+        filepath = r'./test.txt'
+        f = open(filepath, 'r')
+        line = f.readline()
+        print(line+" sent.")
+        arduino.write(str.encode(line))
+          
+        dataRead = arduino.readline()
+        from firebase import firebase
+        firebase = firebase.FirebaseApplication('https://fir-test-7f5bd.firebaseio.com/', None) 
+        result = firebase.patch('/ResponseFromArduino',{'Response':dataRead}) 
+        print(result)
+
+      
+
+      
+
+
+
+
+    # Keep watching in a loop
     def watch(self):
- 
         while self.running:
             try:
+                # Look for changes
                 time.sleep(self.refresh_milliSecond)
                 self.look()
-               
-                    
             except KeyboardInterrupt:
                 print('\nDone')
                 break
@@ -116,7 +81,7 @@ class Watcher(object):
                 print('Unhandled error: %s' % sys.exc_info()[0])
 
 
-watch_file = 'C:\\Users\\robin\\Desktop\\Javascript\\tester12\\test.txt'
+watch_file = r'./test.txt'
 
 # watcher = Watcher(watch_file)  # simple
 watcher = Watcher(watch_file)  # also call custom action function
